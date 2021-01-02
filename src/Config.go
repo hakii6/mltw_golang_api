821d66ad217c6b194b99d44ad3b93a92c0b2847f
hakii6 <ty5220669123@gmail.com>
2021-01-02 18:24:33 +0800

    fixed untracked files

--- src/Config.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const (
-    host     = "localhost"
-    database = "test2"
-    user     = "user"
-    password = "password"
-    connect = user + ":" + password + "@tcp(" + host + ":3306)/" + database + "?charset=utf8&parseTime=true"
-)
