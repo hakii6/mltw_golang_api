849c69be0a57d17fcebf1da9cf0ee923c8a2a051
hakii6 <ty5220669123@gmail.com>
2021-01-05 19:57:30 +0800

    use as service

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/kardianos/service v1.2.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.9
 )
--- src/main.go
+++ src/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	// "encoding/json"
-	// "fmt"
+	_ "fmt"
 	"net/http"
 	"log"
+	"os"
 	"github.com/gorilla/mux"
+	"github.com/kardianos/service"
 )
+var logger service.Logger
 
-func main() {
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+
+func (p *program) run() {
+	// Do work here
 
 	// Init Router
 	Router := mux.NewRouter()
@@ -27,4 +39,46 @@ func main() {
     // })
 
     log.Fatal(http.ListenAndServe(":8001", Router))
+
+}
+
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "GoAPI",
+		DisplayName: "Go API",
+		Description: "Mltw Golang API",
+	}
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if in windows install and start uself
+	if len(os.Args) > 1 {
+		err = service.Control(s, os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if os.Args[1] != "start" {
+			return
+		}
+	}
+
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+
+
 }
